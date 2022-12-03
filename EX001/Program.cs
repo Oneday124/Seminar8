3dbf0869b943fa26a1ac39a92180f0773b690c26
Nikolay <Onedaydejavu124@mail.ru>
2022-12-04 01:04:53 +0700

    Задайте двумерный массив. Напишие программу, которая поменяет местами первую и последнюю строку массива.

--- EX001/Program.cs
+++ EX001/Program.cs
@@ -24,22 +24,16 @@ void PrintArray(int[,] array)
     }
 }
 
-int[,] ReplaceString (int[,] array, int rows)
+int[,] ReplaceString (int[,] array, int cols, int rows)
 {
     int temp = 0;
-    for(int i = 0; i < array.GetLength(0); i++)
+    for(int i = 0; i < cols; i++)
     {
-        for(int j = 0; j < array.GetLength(1); j++)
-        {
-            if(i == 0) 
-            {
-                temp = array[0,j];
-                array[0,j] = array[rows-1,j];
-                array[rows-1,j] = temp;
-            }
-        }
-    }    
-    return array;       
+        temp = array[0,i];
+        array[0,i] = array[rows-1,i];
+        array[rows-1,i] = temp;
+    } 
+    return array; 
 }
 
 int rows = 3;
@@ -50,6 +44,6 @@ FillArray(array);
 PrintArray(array);
 System.Console.WriteLine();
 
-ReplaceString(array, rows);
+ReplaceString(array, cols, rows);
 PrintArray(array);
 
