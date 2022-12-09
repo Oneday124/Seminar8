a78c918ef40a3d708f46f0e6d66df2d9f10ef219
Nikolay <Onedaydejavu124@mail.ru>
2022-12-10 01:22:33 +0700

    Задайте значения m и n. Напишите программу, которая найдет сумму натуральных чисел от m до n

---
+++ EX010/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задайте значения m и n. Напишите программу, которая найдет сумму натуральных чисел от m до n 
+
+int SumOfNumbers(int m, int n)
+{
+    int sum = 0;
+    while(m <= n)
+    {
+        sum += m;
+        m++;
+    }
+    return sum;
+}
+
+
+int m = 4;
+int n = 8;
+System.Console.WriteLine($"{SumOfNumbers(m, n)} ");
