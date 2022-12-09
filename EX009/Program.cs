141a275a63dd5b72449bc96ceb635651c87e6b49
Nikolay <Onedaydejavu124@mail.ru>
2022-12-10 01:16:23 +0700

    Задайте значение N. Напишите программу, которая выведет все натуральные числа от N до 1. Выполнить с помощью рекурсии

---
+++ EX009/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа от N до 1. Выполнить с помощью рекурсии
+
+void EnterNutNumbers(int number)
+{
+    if(number >= 1)
+    {
+        Console.Write($"{number} ");
+        number--;
+        EnterNutNumbers(number);
+    }
+
+}
+
+int number = 15;
+EnterNutNumbers(number);
